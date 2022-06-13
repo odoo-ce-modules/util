61fbbc69c21cf2f924bd220f8bcf27168e38fc90
odoo-ce-modules <67482653+odoo-ce-modules@users.noreply.github.com>
2022-06-13 18:24:02 -0500

    Update export-certs.sh
    Make it better

--- export-certs.sh
+++ export-certs.sh
@@ -1,11 +1,12 @@
-# For an import arg, provide the URL of the target host
-
-openssl s_client -showcerts -verify 5 -connect "$1":443 < /dev/null |
+openssl s_client -showcerts -verify 5 -servername "$1" -connect "$1":443 < /dev/null |
    awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".temp.pem"; print >out}'
 echo
 C=0
 for cert in *.temp.pem; do
   ((C=C+1))
-  newname=$(openssl x509 -noout -subject -in $cert | sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').pem
-  echo "$C-${newname}"; mv "${cert}" "$C-${newname}"
+  newname=$(openssl x509 -noout -subject -in $cert | \
+    sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').pem
+  newname=$(echo "$C-${newname}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
+  echo "${newname}"
+  mv "${cert}" "${newname}"
 done
